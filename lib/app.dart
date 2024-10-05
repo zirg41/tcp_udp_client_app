eed94eb37ae2a1c65ecc8782b58c62a103645851
Faiz Galimov <work.galimov@gmail.com>
2024-10-05 15:34:54 +0300

    move `FlavorBanner` to MaterialApp's builder

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:flutter_gen/gen_l10n/app_localizations.dart";
+import "package:initial_app/core/qa/flavor_banner.dart";
 import "package:initial_app/core/routes/routes.dart";
 
 class App extends StatelessWidget {
@@ -11,6 +12,11 @@ class App extends StatelessWidget {
       routerConfig: appRouter,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
+      builder: (context, child) {
+        return FlavorBanner(
+          child: child ?? const SizedBox.shrink(),
+        );
+      },
     );
   }
 }
--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,7 +1,6 @@
 import "package:flutter/material.dart";
 import "package:initial_app/core/config/environment.dart";
 import "package:initial_app/core/config/injectable.dart";
-import "package:initial_app/core/qa/flavor_banner.dart";
 import "package:initial_app/core/utils/device_info_service.dart";
 import "package:initial_app/core/utils/extensions/build_context_x.dart";
 
@@ -12,25 +11,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return FlavorBanner(
-      child: Scaffold(
-        appBar: AppBar(title: Text(context.strings.appName)),
-        body: Center(
-          child: Column(
-            children: [
-              Text(
-                getIt<Environment>().name,
-              ),
-              FutureBuilder(
-                future: getIt.getAsync<DeviceInfoService>(),
-                builder: (context, snapshot) {
-                  return Text(
-                    "${snapshot.data?.deviceId}",
-                  );
-                },
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(title: Text(context.strings.appName)),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              getIt<Environment>().name,
+            ),
+            FutureBuilder(
+              future: getIt.getAsync<DeviceInfoService>(),
+              builder: (context, snapshot) {
+                return Text(
+                  "${snapshot.data?.deviceId}",
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
